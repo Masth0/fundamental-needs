973888631685c116e4aeddc76aa7ce202dd2e9ef
Masth0 <dev.mastho@gmail.com>
2019-02-25 12:28:43 +0100

    Nodejs

--- README.md
+++ README.md
@@ -10,6 +10,7 @@
 - GIT  
 - Apache  
 - MySQL  
+- Nodejs  
 - PHP modules (php-pear php7.2-curl php7.2-dev php7.2-gd php7.2-mbstring php7.2-zip php7.2-mysql php7.2-xml)  
 - Composer (dependances: wget curl unzip)  
 - PhpStorm  
--- os/dev.sh
+++ os/dev.sh
@@ -29,6 +29,9 @@ install() {
 	## MySQL
 	sudo apt install mysql-server
 
+	## Nodejs
+	sudo snap install node --channel=10/stable --classic
+
 	## Php modules
 	sudo apt install php-pear php7.2-curl php7.2-dev php7.2-gd php7.2-mbstring php7.2-zip php7.2-mysql php7.2-xml
 
