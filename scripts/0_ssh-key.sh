2d035f45462053e4ebb376692fb2306287321ba9
Masth0 <dev.mastho@gmail.com>
2021-07-15 11:16:29 +0200

    oh la boulette

--- scripts/0_ssh-key.sh
+++ scripts/0_ssh-key.sh
@@ -5,14 +5,15 @@
 KEYS=$(ls -la ~/.ssh)
 
 if asking_to_install "Create new ssh key?"; then
-  SSH_FILENAME_DEFAULT=/home/.ssh/$(whoami)/id_rsa
+  SSH_FILENAME_DEFAULT=/home/$(whoami)/.ssh/id_rsa
   echo -e "${BLUE}Your ssh keys:${NOCOLOR}"
   echo -e "${BLUE}${KEYS}${NOCOLOR}"
 
-  read -p "Email for ssh?" SSH_EMAIL
-  read -p "Enter file in which to save the key (${SSH_FILENAME_DEFAULT}):" SSH_FILENAME
-  SSH_FILENAME=${SSH_FILENAME:-SSH_FILENAME_DEFAULT}
-  ssh-keygen -t rsa -C "${SSH_EMAIL}" -f "${SSH_FILENAME}"
+  read -p "Email for ssh? " SSH_EMAIL
+  read -p "Enter file in which to save the key (${SSH_FILENAME_DEFAULT}): " SSH_FILENAME
+  SSH_FILENAME=${SSH_FILENAME:-$SSH_FILENAME_DEFAULT}
+
+  ssh-keygen -t rsa -C $SSH_EMAIL -f $SSH_FILENAME
   # Start ssh-agent
   eval "$(ssh-agent -s)"
   # Add the key to the ssh-agent
