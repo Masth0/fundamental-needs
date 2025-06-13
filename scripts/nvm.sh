2d502fcc4de032c03ab7f419870e188d96c81d7a
Masth0 <dev.mastho@gmail.com>
2025-06-13 11:25:18 +0200

    Récupération de la dernière version de NVM

--- scripts/nvm.sh
+++ scripts/nvm.sh
@@ -2,11 +2,23 @@
 
 . functions.sh
 
+# URL du dépôt GitHub de NVM
+NVM_REPO_URL="https://github.com/nvm-sh/nvm"
+
+# Utiliser curl pour récupérer la dernière version de NVM
+LATEST_VERSION=$(curl -s https://api.github.com/repos/nvm-sh/nvm/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
+
+# Vérifier si la récupération de la version a réussi
+if [ -z "$LATEST_VERSION" ]; then
+  echo "Impossible de récupérer la dernière version de NVM."
+  exit 1
+fi
+
 prompt "Install NVM?"
 result=$?
 
 if [ "$result" -eq 0 ]; then
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+  curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/$LATEST_VERSION/install.sh" | bash
   export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 else
